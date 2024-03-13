d3ea16f28c32475cbcf03323fa4139c022e9e478
Hardy Lutula <hardylutula@gmail.com>
2024-03-13 16:38:16 +0200

    alert added

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,20 @@ import {provideRouter} from '@angular/router'
 import {appRoutes} from './app/app.routes'
 import {provideHttpClient, withInterceptors} from '@angular/common/http'
 import {authInterceptor} from './app/shared/utilities/auth.interceptor'
+import {provideAnimations} from '@angular/platform-browser/animations'
+import {provideToastr} from 'ngx-toastr'
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(appRoutes),
     provideHttpClient(withInterceptors([authInterceptor])),
+    provideAnimations(), // required animations providers
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+      progressAnimation: 'increasing',
+      progressBar: true,
+    }), // Toastr providers
   ],
 })
